d360f9cc271612066ff0a983ec41f1690f78bcdc
mk <majd.khoury93@gmail.com>
2017-12-19 13:17:55 -0500

    Finished getting all the results of the article hits

--- queries.py
+++ queries.py
@@ -1,14 +1,43 @@
-qimport datetime
+import datetime
 import psycopg2
 import bleach
+import csv
 
 """ In this file, we will define the queries,
     and run them one by one. Ideally, we want
     the final product to be displayed on a
     www server hosted as a guest. """
 
+
+def get_slugs():
+    """This function gets the slugs as tuples,
+        parses them into strings, and then is
+        returned as a list called
+        variables_of_slugs in order for us to
+        use in the first query!"""
+    db_name = "news"
+    db = psycopg2.connect(dbname=db_name)
+    c = db.cursor()
+    get_slugs_query = """select slug from articles"""
+    c.execute(get_slugs_query)
+    variables_of_slugs = c.fetchall()
+    for row in variables_of_slugs:
+        print(row)
+    return variables_of_slugs
+
 def first_query():
-    path_variable = "/article/"
+    variables = ['/article/bad-things-gone',
+                 '/article/balloon-goons-doomed',
+                 '/article/bears-love-berries',
+                 '/article/candidate-is-jerk',
+                 '/article/goats-eat-googles',
+                 '/article/media-obsessed-with-bears',
+                 '/article/trouble-for-troubled',
+                 '/article/so-many-bears']
+
+
+    #slugs = get_slugs()
+    #path_variable = "/article/"
     db_name = "news"
     db = psycopg2.connect(dbname=db_name)
     c = db.cursor()
@@ -17,18 +46,29 @@ def first_query():
     join log as l
     on %s = l.path
     group by a.title"""
-    c.execute(first_query,(path_variable + "a.slug", ))
+
+    for row in range(len(variables)):
+        c.execute(first_query, (variables[row],))
+        results = c.fetchone()
+        print(results)
+
+    # c.execute(first_query, (path_variable,))
 
     # Here, the first_query results are stored in a variable
     # by the fetch all command.
     first_query_results = c.fetchall()
 
-    if c.rowcount == 0:
-        print "Query returned no results!"
-    else:
-        for row in first_query_results:
-            print (row)
-    print "First query is complete"
-    db.close()
+    # if c.rowcount == 0:
+    #     print "Query returned no results!"
+    # else:
+    #     for row in first_query_results:
+    #         print(row)
+    #
+    # for row in slugs:
+    #     print(row)
+    #
+    # print "First query is complete"
+    # db.close()
 
 first_query()
+# get_slugs()
