66c217df2eae1c76cce38d6841da164504ac9242
mk <majd.khoury93@gmail.com>
2017-12-19 16:33:35 -0500

    Successfully fetched first 3 rows only

--- queries.py
+++ queries.py
@@ -18,14 +18,10 @@ def first_query():
     from articles as a join log as l
     on l.path like concat('%', a.slug)
     group by a.title
-    order by views desc"""
-
+    order by views desc
+    fetch next 3 rows only"""
     c.execute(first_query)
     first_query_results = c.fetchall()
-    for row in first_query_results:
-        print(row)
-
-
     # Here, the first_query results are stored in a variable
     # by the fetch all command.
 
@@ -37,4 +33,16 @@ def first_query():
     print "First query is complete"
     db.close()
 
+
 first_query()
+
+
+# user_input = input("""Hello and welcome to the Logs analysis
+#       program! Please select the following:
+#       1) Find the top 3 articles of all time
+#       2) Find the top 3 authors of all time
+#       3) Find the day where more than 1 percent
+#       of the requests lead to errors""")
+
+# if user_input == 1:
+#     first_query()
